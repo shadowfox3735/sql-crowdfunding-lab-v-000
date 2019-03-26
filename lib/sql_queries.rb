b0122197b58e0a06b80f38b761f1fb90fab38f1a
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 21:52:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -31,7 +31,8 @@ GROUP BY name ORDER BY SUM(pledges.amount), users.name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT "
+"SELECT projects.category, SUM(pledges.amount) FROM projects
+"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
