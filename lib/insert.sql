d5e712e0a3024ff139cb114fab715c3e0e758e74
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 21:54:04 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,74 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (1, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (2, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (3, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (4, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (5, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (6, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (7, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (8, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (9, null, null, null, null, null);
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
+VALUES (10, null, null, null, null, null);
+
+INSERT INTO users (name,age) 
+VALUES (null,null), 
+(null,null), 
+(null,null), 
+(null,null), 
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null),
+(null,null);
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES (null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null),
+(null,null, null);
