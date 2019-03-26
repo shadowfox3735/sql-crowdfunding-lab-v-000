a58b42b4455bdceabd9e4c210910a4e59f05eee5
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 21:48:54 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -25,7 +25,9 @@ GROUP BY projects.title HAVING SUM(pledges.amount) >= projects.funding_goal"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"SELECT users.name, "
+"SELECT users.name, users.age, SUM(pledges.amount) FROM users
+INNER JOIN pledges ON users.id = pledges.user_id
+GROUP BY name"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
