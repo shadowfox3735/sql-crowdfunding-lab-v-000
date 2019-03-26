321c22481d56c7bda021391b8c6174abe5581e88
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 21:33:05 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,14 +1,14 @@
-INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
 VALUES (1, null, null, null, null, null);
-INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
 VALUES (2, null, null, null, null, null);
-INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
 VALUES (3, null, null, null, null, null);
-INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
 VALUES (4, null, null, null, null, null);
-INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
 VALUES (5, null, null, null, null, null);
-INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date)
 VALUES (6, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
 VALUES (7, null, null, null, null, null);
