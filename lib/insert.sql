fc274790516d06c432e5e7e5162a121447eb51eb
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 21:28:44 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,21 +1,22 @@
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (1, null, null, null, null, null)
+VALUES (1, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (2, null, null, null, null, null)
+VALUES (2, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (3, null, null, null, null, null)
+VALUES (3, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (4, null, null, null, null, null)
+VALUES (4, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (5, null, null, null, null, null)
+VALUES (5, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (6, null, null, null, null, null)
+VALUES (6, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (7, null, null, null, null, null)
+VALUES (7, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (8, null, null, null, null, null)
+VALUES (8, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (9, null, null, null, null, null)
+VALUES (9, null, null, null, null, null);
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
-VALUES (10, null, null, null, null, null)
+VALUES (10, null, null, null, null, null);
 
+INSERT INTO users (name,age) VALUES (null,null)
