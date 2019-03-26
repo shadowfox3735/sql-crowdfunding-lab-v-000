0dd36fd97d3b3f4e8ce2382a5c3615cbf0f75829
shadowfox3735 <normanpan180@gmail.com>
2019-03-26 21:23:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,4 @@
 INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
 VALUES 
+INSERT INTO projects (id, title, category, funding_goal, starting_date, end_date)
+VALUES 
